339dd15622942b1ce896256a27be4390e3082487
Boyko Nikolaev <boykonikolaev1997@gmail.com>
2023-05-14 20:27:02 +0300

    Seed data and updates to ProductsController

--- Infrastructure/Data/ProductRepository.cs
+++ Infrastructure/Data/ProductRepository.cs
@@ -17,10 +17,29 @@ namespace Infrastructure.Data
             this._context = context;
         }
 
+        public async Task<IReadOnlyList<ProductBrand>> GetProductBrandsAsync()
+        {
+            return await this._context.ProductBrands.ToListAsync();
+        }
+
         public async Task<Product> GetProductByIdAsync(int id)
-            => await this._context.Products.FindAsync(id);
+        {
+            return await this._context.Products
+                .Include(p => p.ProductType)
+                .Include(p => p.ProductBrand)
+                .FirstOrDefaultAsync(p => p.Id == id);
+        }
 
         public async Task<IReadOnlyList<Product>> GetProductsAsync()
-            => await this._context.Products.ToListAsync();
+        {
+            return await this._context.Products
+                .Include(p => p.ProductType)
+                .Include(p => p.ProductBrand)
+                .ToListAsync();
+        }
+        public async Task<IReadOnlyList<ProductType>> GetProductTypesAsync()
+        {
+            return await this._context.ProductTypes.ToListAsync();
+        }
     }
 }
