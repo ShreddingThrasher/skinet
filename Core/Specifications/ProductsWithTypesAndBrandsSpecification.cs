55d78591f29a9516d464e62ac2ce9c6d37af85c3
Boyko Nikolaev <100221813+ShreddingThrasher@users.noreply.github.com>
2023-05-17 19:31:11 +0300

    changes in specification

--- Core/Specifications/ProductsWithTypesAndBrandsSpecification.cs
+++ Core/Specifications/ProductsWithTypesAndBrandsSpecification.cs
@@ -9,13 +9,30 @@ namespace Core.Specifications
 {
     public class ProductsWithTypesAndBrandsSpecification : BaseSpecification<Product>
     {
-        public ProductsWithTypesAndBrandsSpecification()
+        public ProductsWithTypesAndBrandsSpecification(string sort)
         {
             this.AddInclude(x => x.ProductType);
             this.AddInclude(x => x.ProductBrand);
+            this.AddOrderBy(x => x.Name);
+
+            if (!string.IsNullOrEmpty(sort))
+            {
+                switch (sort)
+                {
+                    case "priceAsc":
+                        this.AddOrderBy(p => p.Price);
+                        break;
+                    case "priceDesc":
+                        this.AddOrderByDescending(p => p.Price);
+                        break;
+                    default:
+                        this.AddOrderBy(p => p.Name);
+                        break;
+                }
+            }
         }
 
-        public ProductsWithTypesAndBrandsSpecification(int id) 
+        public ProductsWithTypesAndBrandsSpecification(int id)
             : base(x => x.Id == id)
         {
             this.AddInclude(x => x.ProductType);
