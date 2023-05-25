f29124da505719dbb3a5f6a1950557fd2bfedf98
Boyko Nikolaev <100221813+ShreddingThrasher@users.noreply.github.com>
2023-05-25 21:40:57 +0300

    pagination fixed

--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -19,9 +19,11 @@ export class ShopService {
 
     let params = new HttpParams();
 
-    if(shopParams.brandId !== 0) params = params.append('brandId', shopParams.brandId);
-    if(shopParams.typeId !== 0) params = params.append('typeId', shopParams.typeId);
+    if(shopParams.brandId > 0) params = params.append('brandId', shopParams.brandId);
+    if(shopParams.typeId > 0) params = params.append('typeId', shopParams.typeId);
     params = params.append('sort', shopParams.sort);
+    params = params.append('pageIndex', shopParams.pageNumber);
+    params = params.append('pageSize', shopParams.pageSize);
 
     return this.http.get<Pagination<Product>>(this.baseUrl + 'products', {params: params});
   }
