7452dab95c813e09699d37c4abdf32569975c7f0
Boyko Nikolaev <boykonikolaev1997@gmail.com>
2023-05-26 18:57:35 +0300

    loading spinner in front end

--- client/src/app/shop/product-details/product-details.component.ts
+++ client/src/app/shop/product-details/product-details.component.ts
@@ -14,7 +14,9 @@ export class ProductDetailsComponent implements OnInit {
   product?: Product;
 
   constructor(private shopService: ShopService, private activatedRoute: ActivatedRoute, 
-    private bcService: BreadcrumbService) {}
+    private bcService: BreadcrumbService) {
+      this.bcService.set('@productDetails', ' ');
+    }
 
   ngOnInit(): void {
     this.loadProduct();
@@ -23,7 +25,7 @@ export class ProductDetailsComponent implements OnInit {
   loadProduct(){
     const id = this.activatedRoute.snapshot.paramMap.get('id');
 
-    if(id) this.shopService.getProduct(+id).subscribe({
+    if (id) this.shopService.getProduct(+id).subscribe({
       next: product => {
         this.product = product;
         this.bcService.set('@productDetails', product.name);
