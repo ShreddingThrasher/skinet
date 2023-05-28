a04a41b5bf09a544b76d949eaaa500fa936fe4c9
Boyko Nikolaev <boykonikolaev1997@gmail.com>
2023-05-28 18:10:08 +0300

    basket functionality and product details functions

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,14 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { BasketService } from './basket/basket.service';
+
+type NewType = OnInit;
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss']
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-    title = 'Skinet';
+export class AppComponent implements NewType {
+  title = 'Skinet';
 
-    constructor() { }
+  constructor(private basketService: BasketService) { }
 
-    
+  ngOnInit(): void {
+    const basketId = localStorage.getItem('basket_id');
+
+    if(basketId) this.basketService.getBasket(basketId);
+  }
 }
